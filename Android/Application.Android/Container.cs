d2a9a37693aa093e048d1bce11a1c6ca4b51a2d9
Julien Mialon <mialon.julien@gmail.com>
2015-04-08 10:50:36 +0200

    Fixing issue with translation, add init of LocalizationService + new nuget packages.

--- Android/Application.Android/Container.cs
+++ Android/Application.Android/Container.cs
@@ -1,14 +1,25 @@
-﻿using IndiaRose.Business;
+﻿using System;
+using System.Collections.Generic;
+using IndiaRose.Business;
 using IndiaRose.Interfaces;
 using IndiaRose.Services;
 using IndiaRose.Services.Android;
 using Storm.Mvvm.Inject;
+using Storm.Mvvm.Services;
 
 namespace IndiaRose.Application
 {
 	public class Container : AndroidContainer
 	{
         public readonly static ViewModelsLocator Locator = new ViewModelsLocator();
+
+		protected override void Initialize(Android.App.Application application, Dictionary<string, Type> views, Dictionary<string, Type> dialogs)
+		{
+			base.Initialize(application, views, dialogs);
+
+			ILocalizationService localizationService = new LocalizationService(application, typeof(Resource.String));
+			RegisterInstance(localizationService);
+		}
 
 		protected override void Initialize()
 		{
