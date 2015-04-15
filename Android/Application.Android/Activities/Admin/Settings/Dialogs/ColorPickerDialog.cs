f5048227ae4b6ace03d0e38c18cc8e9583b05f6f
Valentin <val_jub@live.fr>
2015-04-15 16:09:19 +0200

    trad color picker + bg image (marche pas suivant les résolutions)

--- Android/Application.Android/Activities/Admin/Settings/Dialogs/ColorPickerDialog.cs
+++ Android/Application.Android/Activities/Admin/Settings/Dialogs/ColorPickerDialog.cs
@@ -3,7 +3,10 @@
 using IndiaRose.Framework.Converters;
 using Storm.Mvvm;
 using Storm.Mvvm.Bindings;
+using Storm.Mvvm.Dialogs;
 using Storm.Mvvm.Framework.ColorPicker;
+using Storm.Mvvm.Inject;
+using Storm.Mvvm.Services;
 
 #endregion
 
@@ -16,7 +19,16 @@ namespace IndiaRose.Application.Activities.Admin.Settings.Dialogs
 	{
 		protected override ViewModelBase CreateViewModel()
 		{
-			return Container.Locator.AdminSettingsDialogsColorPickerViewModel;
+		    return Container.Locator.AdminSettingsDialogsColorPickerViewModel;
 		}
+
+	    public ColorPickerDialog()
+        {
+            var trad = DependencyService.Container.Resolve<ILocalizationService>();
+            Buttons[DialogsButton.Positive] = trad.GetString("Button_Ok", "Text");
+            Buttons[DialogsButton.Negative] = trad.GetString("Button_Back", "Text");
+            //TODO le string pour le titre du colorpicker
+            //Title = trad.GetString()
+	    }
 	}
 }
