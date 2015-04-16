7f773f7785a45f3ef14a5559a1862c212a7a8fec
Corentin-Colomier <ccolomier@gmail.com>
2015-04-16 16:15:21 +0200

    CollectionManagementPage avent le fragment

--- Android/Application.Android/Activities/Admin/Collection/CollectionManagementActivity.cs
+++ Android/Application.Android/Activities/Admin/Collection/CollectionManagementActivity.cs
@@ -1,18 +1,12 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+
 using Storm.Mvvm;
 using Android.App;
-using Android.Content;
+using Android.Content.PM;
 using Android.OS;
-using Android.Runtime;
-using Android.Views;
-using Android.Widget;
 
 namespace IndiaRose.Application.Activities.Admin.Collection
 {
-    [Activity(Label = "CollectionManagementActivity")]
+    [Activity(ScreenOrientation = ScreenOrientation.Landscape, Theme = "@style/Theme.Sherlock.Light.NoActionBar")]
     public partial class CollectionManagementActivity : ActivityBase
     {
         protected override void OnCreate(Bundle bundle)
