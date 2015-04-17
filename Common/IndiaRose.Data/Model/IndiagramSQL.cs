27a4463c70ab2816c53f7d365bf6f6376d4ed2ef
David-intho <david.intho@gmail.com>
2015-04-17 14:24:14 +0200

    correction commentaire

--- Common/IndiaRose.Data/Model/IndiagramSQL.cs
+++ Common/IndiaRose.Data/Model/IndiagramSQL.cs
@@ -1,4 +1,4 @@
-﻿using System;
+﻿/*using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -46,7 +46,7 @@ namespace IndiaRose.Data.Model
             }
             else
             {
-                /*null ?*/
+                null ?
             }
         }
 
@@ -59,3 +59,4 @@ namespace IndiaRose.Data.Model
     
 
 }
+*/
