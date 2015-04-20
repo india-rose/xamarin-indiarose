94a2f3042ca6145241fd2af102efd83e3660e297
Valentin <val_jub@live.fr>
2015-04-20 16:25:47 +0200

    début binding indiagram

--- Android/Application.Android/Activities/Admin/Collection/WatchIndiagramActivity.cs
+++ Android/Application.Android/Activities/Admin/Collection/WatchIndiagramActivity.cs
@@ -1,11 +1,14 @@
 using Android.App;
 using Android.Content.PM;
 using Android.OS;
+using Android.Widget;
+using IndiaRose.Business.ViewModels.Admin.Collection;
+using IndiaRose.Data.Model;
 using Storm.Mvvm;
 
 namespace IndiaRose.Application.Activities.Admin.Collection
 {
-     [Activity(ScreenOrientation = ScreenOrientation.Landscape, Theme = "@style/Theme.Sherlock.Light.NoActionBar")]
+    [Activity(ScreenOrientation = ScreenOrientation.Landscape, Theme = "@style/Theme.Sherlock.Light.NoActionBar")]
     public partial class WatchIndiagramActivity : ActivityBase
     {
         protected override void OnCreate(Bundle bundle)
@@ -14,6 +17,22 @@ namespace IndiaRose.Application.Activities.Admin.Collection
 
             SetContentView(Resource.Layout.Admin_Collection_WatchIndiagramPage);
             SetViewModel(Container.Locator.AdminCollectionWatchIndiagramViewModel);
+
+            Initialize();
+        }
+
+        private void Initialize()
+        {
+            WatchIndiagramViewModel vm = (WatchIndiagramViewModel)ViewModel;
+            TextView nameTextView = FindViewById<TextView>(Resource.Id.text1);
+            nameTextView.Text = vm.CurrentIndiagram.Text;
+            TextView parentTextView = FindViewById<TextView>(Resource.Id.text2);
+            Indiagram parent = vm.CurrentIndiagram.Parent;
+            if (parent != null)
+                parentTextView.Text = parent.Text;
+            TextView soundpathTextView = FindViewById<TextView>(Resource.Id.text3);
+            soundpathTextView.Text = vm.CurrentIndiagram.SoundPath ?? "Aucun";
+
         }
     }
 }
--- Common/IndiaRose.Business/ViewModels/Admin/Collection/WatchIndiagramViewModel.cs
+++ Common/IndiaRose.Business/ViewModels/Admin/Collection/WatchIndiagramViewModel.cs
@@ -3,15 +3,27 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
+using IndiaRose.Data.Model;
+using Storm.Mvvm.Commands;
 using Storm.Mvvm.Inject;
 
 namespace IndiaRose.Business.ViewModels.Admin.Collection
 {
     public class WatchIndiagramViewModel : AbstractViewModel
     {
+        public ICommand EditCommand { get; private set; }
+        public Indiagram CurrentIndiagram { get; private set; }
+
          public WatchIndiagramViewModel(IContainer container)
             : base(container)
         {
+             EditCommand=new DelegateCommand(EditAction);
+             CurrentIndiagram=new Indiagram("test",null);
         }
+         private void EditAction()
+         {
+             NavigationService.Navigate(Views.ADMIN_COLLECTION_ADD);
+         }
     }
 }
