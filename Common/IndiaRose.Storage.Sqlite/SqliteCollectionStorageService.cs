18fa692aed210ed70271a94533208bf0f5803153
Julien Gauttier <julien.gauttier@gmail.com>
2015-04-20 15:50:53 +0200

    GetFullCollection

--- Common/IndiaRose.Storage.Sqlite/SqliteCollectionStorageService.cs
+++ Common/IndiaRose.Storage.Sqlite/SqliteCollectionStorageService.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using IndiaRose.Data.Model;
+using IndiaRose.Storage.Sqlite.Model;
+using SQLite.Net.Async;
 
 namespace IndiaRose.Storage.Sqlite
 {
@@ -8,7 +10,7 @@ namespace IndiaRose.Storage.Sqlite
 	{
 		public void Create(Indiagram indiagram)
 		{
-			throw new NotImplementedException();
+            throw new NotImplementedException();
 		}
 
 		public void Update(Indiagram indiagram)
@@ -33,7 +35,27 @@ namespace IndiaRose.Storage.Sqlite
 
 		public List<Indiagram> GetFullCollection()
 		{
-			throw new NotImplementedException();
+		    var list = new List<Indiagram>();
+		    foreach (var t in GetTopLevel())
+		    {
+		        list = AddChildren(list, t);
+		    }
+		    return list;
+
 		}
+
+	    public List<Indiagram> AddChildren(List<Indiagram> list, Indiagram indiagram)
+	    {
+	        if (!(indiagram is Category)) return list;
+	        foreach (var t in indiagram.Children)
+	        {
+	            list.Add(t);
+	            if (t is Category)
+	            {
+	                return AddChildren(list, t);
+	            }
+	        }
+	        return list;
+	    }
 	}
 }
