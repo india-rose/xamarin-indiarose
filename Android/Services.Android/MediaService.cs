92615c139320d0771d44c08aa588bbb471563b8d
David-intho <david.intho@gmail.com>
2015-04-22 10:42:42 +0200

    déplacement méthode enregistrement son

--- Android/Services.Android/MediaService.cs
+++ Android/Services.Android/MediaService.cs
@@ -1,18 +1,33 @@
 using System;
+using Android.Media;
 using IndiaRose.Interfaces;
 
 namespace IndiaRose.Services.Android
 {
     public class MediaService : IMedia
     {
+
+        private MediaRecorder _recorder;
+        private String _url;
+
         public void StartWrite(string path)
         {
-            throw new NotImplementedException();
+            _url = "/sdcard/IndiaRose/sound/" + path + ".3gpp";
+            _recorder = new MediaRecorder();
+            _recorder.SetAudioSource(AudioSource.Mic);
+            _recorder.SetOutputFormat(OutputFormat.ThreeGpp);
+            _recorder.SetAudioEncoder(AudioEncoder.AmrNb);
+            _recorder.SetOutputFile(_url);
+            _recorder.Prepare();
+            _recorder.Start();
         }
 
         public string StopWrite()
         {
-            throw new NotImplementedException();
+            _recorder.Stop();
+            _recorder.Reset();
+
+            return _url;
         }
 
         public void StartRead(string path)
@@ -20,7 +35,7 @@ namespace IndiaRose.Services.Android
             throw new NotImplementedException();
         }
 
-        public string StopRead(Uri data)
+        public string StopRead(string data)
         {
             throw new NotImplementedException();
         }
