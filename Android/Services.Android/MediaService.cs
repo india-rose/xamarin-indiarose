4512006aa4b3623a24bd4ec1a19c579032f2b14c
Valentin <val_jub@live.fr>
2015-04-22 10:38:09 +0200

    ajout mediaservice

---
+++ Android/Services.Android/MediaService.cs
@@ -0,0 +1,28 @@
+using System;
+using IndiaRose.Interfaces;
+
+namespace IndiaRose.Services.Android
+{
+    public class MediaService : IMedia
+    {
+        public void StartWrite(string path)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string StopWrite()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void StartRead(string path)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string StopRead(Uri data)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
