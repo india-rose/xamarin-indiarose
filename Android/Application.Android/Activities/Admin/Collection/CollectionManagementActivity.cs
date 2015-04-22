1bdee6b2d54267ed563f44042513fb79068c2265
Corentin-Colomier <ccolomier@gmail.com>
2015-04-22 18:17:35 +0200

    fragment

--- Android/Application.Android/Activities/Admin/Collection/CollectionManagementActivity.cs
+++ Android/Application.Android/Activities/Admin/Collection/CollectionManagementActivity.cs
@@ -1,15 +1,18 @@
 
+using System.Collections.Generic;
 using Android.App;
 using Android.Content.PM;
 using Android.OS;
+using IndiaRose.Data.Model;
 using Storm.Mvvm;
+using Storm.Mvvm.Bindings;
 
 namespace IndiaRose.Application.Activities.Admin.Collection
 {
     [Activity(ScreenOrientation = ScreenOrientation.Landscape, Theme = "@style/Theme.Sherlock.Light.NoActionBar")]
     public partial class CollectionManagementActivity : ActivityBase
     {
-        protected override void OnCreate(Bundle bundle)
+		protected override void OnCreate(Bundle bundle)
         {
             base.OnCreate(bundle);
 
