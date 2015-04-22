8b8ffbee86f3968124004595590da510eee01b45
Corentin-Colomier <ccolomier@gmail.com>
2015-04-22 15:14:18 +0200

    avec l'affichage du fragment

--- Common/IndiaRose.Business/ViewModels/Admin/Collection/ManagementViewModel.cs
+++ Common/IndiaRose.Business/ViewModels/Admin/Collection/ManagementViewModel.cs
@@ -1,12 +1,19 @@
-﻿using Storm.Mvvm.Inject;
+﻿using System.Collections.Generic;
+using IndiaRose.Business.ViewModels.Admin.Settings;
+using IndiaRose.Data.Model;
+using Storm.Mvvm.Inject;
+using Storm.Mvvm.Navigation;
 
 namespace IndiaRose.Business.ViewModels.Admin.Collection
 {
-    public class ManagementViewModel : AbstractViewModel
+    public class ManagementViewModel : AbstractSettingsViewModel
     {
+        public List<Indiagram> CurrentIndiagram { get; private set; }
+
         public ManagementViewModel(IContainer container)
             : base(container)
         {
+
         }
     }
 }
