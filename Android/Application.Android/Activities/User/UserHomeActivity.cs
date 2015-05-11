cf3cb11aa3b66036b5028f3c3eb732d92d332080
Valentin <val_jub@live.fr>
2015-05-11 11:32:48 +0200

    page user suite

--- Android/Application.Android/Activities/User/UserHomeActivity.cs
+++ Android/Application.Android/Activities/User/UserHomeActivity.cs
@@ -1,20 +1,11 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-
-using Android.App;
-using Android.Content;
-using Android.Content.PM;
-using Android.OS;
-using Android.Runtime;
-using Android.Views;
-using Android.Widget;
-using Storm.Mvvm;
+  using Android.App;
+  using Android.Content.PM;
+  using Android.OS;
+  using Storm.Mvvm;
 
 namespace IndiaRose.Application.Activities.User
 {
-    //[Activity(Label = "India Rose User Dev", MainLauncher = true, Icon = "@drawable/ir_logo", ScreenOrientation = ScreenOrientation.Landscape, Theme = "@style/Theme.Sherlock.Light.NoActionBar")]
+    [Activity(Label = "India Rose User Dev", Icon = "@drawable/ir_logo", Theme = "@style/Theme.Sherlock.Light.NoActionBar", MainLauncher = true, ScreenOrientation = ScreenOrientation.Landscape)]
     public partial class UserHomeActivity : ActivityBase
     {
         protected override void OnCreate(Bundle bundle)
