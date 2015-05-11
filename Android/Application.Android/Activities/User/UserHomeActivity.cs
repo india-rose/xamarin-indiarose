a4852c82844acda5cf9e8efdb9de4f6c2c6d85e0
Julien Mialon <mialon.julien@gmail.com>
2015-05-11 16:00:04 +0200

    Fix some init issue with front

--- Android/Application.Android/Activities/User/UserHomeActivity.cs
+++ Android/Application.Android/Activities/User/UserHomeActivity.cs
@@ -4,41 +4,85 @@ using Android.Content.PM;
 using Android.OS;
 using Android.Views;
 using IndiaRose.Interfaces;
-using IndiaRose.Services.Android;
 using Storm.Mvvm;
 using Storm.Mvvm.Inject;
 
 namespace IndiaRose.Application.Activities.User
 {
-    //[Activity(Label = "India Rose User Dev", Icon = "@drawable/ir_logo", Theme = "@style/Theme.Sherlock.Light.NoActionBar", MainLauncher = true, ScreenOrientation = ScreenOrientation.Landscape, LaunchMode = LaunchMode.SingleTask)]
-    public partial class UserHomeActivity : ActivityBase
-    {
-        protected ISettingsService SettingsService { get { return LazyResolver<ISettingsService>.Service; } }
+	[Activity(Label = "India Rose User Dev", Icon = "@drawable/ir_logo", Theme = "@style/Theme.Sherlock.Light.NoActionBar", MainLauncher = true, ScreenOrientation = ScreenOrientation.Landscape,
+		LaunchMode = LaunchMode.SingleTask)]
+	public partial class UserHomeActivity : ActivityBase
+	{
+		protected ISettingsService SettingsService
+		{
+			get { return LazyResolver<ISettingsService>.Service; }
+		}
 
-        protected override void OnCreate(Bundle bundle)
-        {
-            base.OnCreate(bundle);
-            SettingsService.Loaded+=SettingsService_Loaded;
-            SetViewModel(Container.Locator.UserHomeViewModel);
-        }
+		private bool _initialized;
+		private readonly object _mutex = new object();
 
-        private void SettingsService_Loaded(object sender, EventArgs e)
-        {
-            SetContentView(Resource.Layout.User_HomePage);
-            Initialize();
-        }
+		protected override void OnCreate(Bundle bundle)
+		{
+			base.OnCreate(bundle);
+			SettingsService.Loaded += SettingsService_Loaded;
+			lock (_mutex)
+			{
+				if (SettingsService.IsLoaded && !_initialized)
+				{
+					SettingsService.Loaded -= SettingsService_Loaded;
+					_initialized = true;
+					SetUp();
+				}
+			}
+		}
 
-        private void Initialize()
-        {
+		private void SettingsService_Loaded(object sender, EventArgs e)
+		{
+			lock (_mutex)
+			{
+				if (!_initialized)
+				{
+					SettingsService.Loaded -= SettingsService_Loaded;
+					_initialized = true;
+					SetUp();
+				}
+			}
+		}
 
-            int height = title_bar.LayoutParameters.Height;
+		private void SetUp()
+		{
+			SetContentView(Resource.Layout.User_HomePage);
+			SetViewModel(Container.Locator.UserHomeViewModel);
 
-            ViewGroup.LayoutParams top = top_indiabrowser.LayoutParameters;
-            top.Height = height * SettingsService.SelectionAreaHeight;
-            top_indiabrowser.LayoutParameters = top;
-            ViewGroup.LayoutParams bottom = bot_sentencearea.LayoutParameters;
-            bottom.Height = height * (1 - SettingsService.SelectionAreaHeight);
-            bot_sentencearea.LayoutParameters = bottom;
-        }
-    }
+			RootLayout.LayoutChange += OnLayoutChange;
+		}
+
+		private void OnLayoutChange(object sender, View.LayoutChangeEventArgs layoutChangeEventArgs)
+		{
+			if (RootLayout.Height > 0)
+			{
+				RootLayout.LayoutChange -= OnLayoutChange;
+				Initialize();
+			}
+		}
+
+		private void Initialize()
+		{
+			int availableHeight = RootLayout.Height - TitleBar.Height;
+
+			ViewGroup.LayoutParams top = IndiagramBrowser.LayoutParameters;
+			top.Height = (int)(availableHeight*(SettingsService.SelectionAreaHeight / 100.0));
+			IndiagramBrowser.LayoutParameters = top;
+
+
+			var sentenceArea1 = FindViewById<Framework.Views.SentenceArea>(Resource.Id.SentenceArea);
+			var sentenceArea2 = FindViewById(Resource.Id.SentenceArea);
+
+			/*
+			ViewGroup.LayoutParams bottom = SentenceArea.LayoutParameters;
+			bottom.Height = (int)(availableHeight*(1 - SettingsService.SelectionAreaHeight / 100.0));
+			SentenceArea.LayoutParameters = bottom;
+			 */
+		}
+	}
 }
