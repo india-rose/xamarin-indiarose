34b26947c87bd9e36a7f62085f4997a7df9f5447
Corentin-Colomier <ccolomier@gmail.com>
2015-05-12 16:36:07 +0200

    recupere les fichier perdu dans les merge

---
+++ Windows8/Services.Tablet/ResourceService.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.IO.Compression;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.Storage;
+using IndiaRose.Interfaces;
+
+namespace IndiaRose.Services
+{
+	public class ResourceService : IResourceService
+	{
+
+		private async void UnZipFile(string zipFileName)
+		{
+			var folder = ApplicationData.Current.LocalFolder;
+
+			using (var zipStream = await folder.OpenStreamForReadAsync("zipFileName"))
+			{
+				using (MemoryStream zipMemoryStream = new MemoryStream((int) zipStream.Length))
+				{
+					await zipStream.CopyToAsync(zipMemoryStream);
+
+					using (var archive = new ZipArchive(zipMemoryStream, ZipArchiveMode.Read))
+					{
+						foreach (ZipArchiveEntry entry in archive.Entries)
+						{
+							if (entry.Name != "")
+							{
+								using (Stream fileData = entry.Open())
+								{
+									StorageFile outputFile = await folder.CreateFileAsync(entry.Name, CreationCollisionOption.ReplaceExisting);
+									using (Stream outputFileStream = await outputFile.OpenStreamForWriteAsync())
+									{
+										await fileData.CopyToAsync(outputFileStream);
+										await outputFileStream.FlushAsync();
+									}
+								}
+							}
+						}
+					}
+				}
+			}
+		}
+		/*
+		public void Copy(string src, string dest)
+		{
+			FileStream output = File.OpenWrite(dest);
+			Stream input = CurrentActivity.Assets.Open(src);
+			input.CopyTo(output);
+			input.Close();
+			output.Flush();
+			output.Close();
+		}
+		*/
+	}
+
+}
