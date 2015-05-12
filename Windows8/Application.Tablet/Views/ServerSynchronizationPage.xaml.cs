eb0afe220732814ca32be4b91058ed58f321fcc6
Julien Gauttier <julien.gauttier@gmail.com>
2015-05-12 16:13:02 +0200

    ServerSync res dial & styles

--- Windows8/Application.Tablet/Views/ServerSynchronizationPage.xaml.cs
+++ Windows8/Application.Tablet/Views/ServerSynchronizationPage.xaml.cs
@@ -20,9 +20,9 @@ namespace IndiaRose.Application.Views
     /// <summary>
     /// Une page vide peut être utilisée seule ou constituer une page de destination au sein d'un frame.
     /// </summary>
-    public sealed partial class BlankPage1 : Page
+    public sealed partial class ServerSynchronizationPage : Page
     {
-        public BlankPage1()
+        public ServerSynchronizationPage()
         {
             this.InitializeComponent();
         }
