df40a18707b1b07e5077c93c456881e44f7c3822
Julien Gauttier <julien.gauttier@gmail.com>
2015-05-20 16:57:53 +0200

    merge

---
+++ Windows8/Framework.Tablet/Converters/BooleanToVisibilityConverter.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Xaml;
+
+namespace IndiaRose.Framework.Converters
+{
+    class BooleanToVisibilityConverter
+    {
+        public object Convert(object value)
+        {
+            return (bool)value ? Visibility.Visible : Visibility.Collapsed;
+        }
+    }
+}
