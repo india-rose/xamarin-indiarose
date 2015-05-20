243cb5dd44fde0771376b9419f33d1f14d6284f5
Corentin-Colomier <ccolomier@gmail.com>
2015-05-20 10:16:25 +0200

    suit WatchIndiagramPage

--- Windows8/Application.Tablet/Views/Admin/Collection/WatchIndiagramPage.xaml.cs
+++ Windows8/Application.Tablet/Views/Admin/Collection/WatchIndiagramPage.xaml.cs
@@ -20,7 +20,7 @@ namespace IndiaRose.Application.Views.Admin.Collection
 	/// <summary>
 	/// Une page vide peut être utilisée seule ou constituer une page de destination au sein d'un frame.
 	/// </summary>
-	public sealed partial class WatchIndiagramPage : Page
+	public sealed partial class WatchIndiagramPage 
 	{
 		public WatchIndiagramPage()
 		{
