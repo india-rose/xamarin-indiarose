22a213ea315234caa5cd72c7cf273259a8554586
Julien Gauttier <julien.gauttier@gmail.com>
2015-05-22 14:30:13 +0200

    Reinforcer and converter

--- Windows8/Framework.Tablet/Converters/IndiaSizeToMarginSize.cs
+++ Windows8/Framework.Tablet/Converters/IndiaSizeToMarginSize.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,12 +8,12 @@ using Windows.UI.Xaml.Data;
 
 namespace IndiaRose.Framework.Converters
 {
-    public class IndiaSizeToReinforcerSizeConverter : IValueConverter
+    public class IndiaSizeToMarginSize : IValueConverter
     {
-
         public object Convert(object value, Type targetType, object parameter, string language)
         {
-            return (int)value*1.2;
+            int x =(int) value;
+            return (int) ((x*1.2) - x)/2;
         }
 
         public object ConvertBack(object value, Type targetType, object parameter, string language)
