cf622820cf521e500bd33f5fb28b3ddc99abccf3
David-intho <david.intho@gmail.com>
2015-05-22 10:34:28 +0200

    Add ImageChoiceDialog

--- Windows8/Application.Tablet/CompositionRoot/Bootstrap.cs
+++ Windows8/Application.Tablet/CompositionRoot/Bootstrap.cs
@@ -37,7 +37,8 @@ namespace IndiaRose.Application.CompositionRoot
                 {Dialogs.ADMIN_SETTINGS_READINGDELAY, typeof(ReadingDelayDialog)},
                 {Dialogs.ADMIN_SETTINGS_COLORPICKER, typeof(ColorPickerDialog)},
                 {Dialogs.ADMIN_MAILERROR,typeof(MailErrorDialog)},
-                {Dialogs.IMPORTING_COLLECTION, typeof(ImportingCollectionDialog)}
+                {Dialogs.IMPORTING_COLLECTION, typeof(ImportingCollectionDialog)},
+                {Dialogs.ADMIN_COLLECTION_IMAGECHOICE, typeof(ImageChoiceDialog)}
             };
 
 
