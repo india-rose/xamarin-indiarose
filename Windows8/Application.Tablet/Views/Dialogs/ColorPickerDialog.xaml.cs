e2c4355e5fae19abbbf61ab57f747d18125d4ab3
Julien Gauttier <julien.gauttier@gmail.com>
2015-05-27 16:50:29 +0200

    ColorPicker with color dialog

--- Windows8/Application.Tablet/Views/Dialogs/ColorPickerDialog.xaml.cs
+++ Windows8/Application.Tablet/Views/Dialogs/ColorPickerDialog.xaml.cs
@@ -6,6 +6,7 @@ using System.Reflection;
 using System.Runtime.InteropServices.WindowsRuntime;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using Windows.Storage.Pickers;
 using Windows.UI;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
@@ -14,6 +15,10 @@ using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
+using IndiaRose.Business.ViewModels;
+using IndiaRose.Business.ViewModels.Admin.Settings.Dialogs;
+using IndiaRose.Interfaces;
+using Storm.Mvvm.Inject;
 
 // Pour en savoir plus sur le modèle d'élément Page vierge, consultez la page http://go.microsoft.com/fwlink/?LinkId=234238
 
@@ -24,14 +29,39 @@ namespace IndiaRose.Application.Views.Dialogs
     /// </summary>
     public sealed partial class ColorPickerDialog
     {
-        public ColorPickerDialog() : base (700)
+        public ColorPickerDialog()
+            : base(700)
         {
             this.InitializeComponent();
             Color.colorChanged += (object sender, EventArgs args) =>
             {
                 // Main Grid 
                 MainGridLayout.Background = new SolidColorBrush(Color.SelectedColor);
-            };    
+                Grid.Background = new SolidColorBrush(Color.SelectedColor);
+                var black = new SolidColorBrush(Colors.Black);
+                var white = new SolidColorBrush(Colors.White);
+                if (TooLight(Color.SelectedColor))
+                {
+                    Title.Foreground =
+                        Expl.Foreground =
+                            Ok.Foreground = Ok.BorderBrush = Cancel.Foreground = Cancel.BorderBrush = black;
+                }
+                else
+                {
+                    Title.Foreground =
+                        Expl.Foreground =
+                            Ok.Foreground = Ok.BorderBrush = Cancel.Foreground = Cancel.BorderBrush = white;
+                }
+            };
+        }
+
+        private bool TooLight(Color color)
+        {
+            if ((color.B > 0x09) && (color.G > 0x09) && (color.R > 0x09))
+            {
+                return true;
+            }
+            return false;
         }
 
         public Color BorderColor { get; set; }
