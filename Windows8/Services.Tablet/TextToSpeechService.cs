9bda724a86b032029a4e67d253c6076e2be1c1d4
Valentin <val_jub@live.fr>
2015-05-29 09:08:37 +0200

    tts

--- Windows8/Services.Tablet/TextToSpeechService.cs
+++ Windows8/Services.Tablet/TextToSpeechService.cs
@@ -3,18 +3,31 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Windows.Media.SpeechSynthesis;
+using Windows.UI.Xaml.Controls;
 using IndiaRose.Interfaces;
 
 namespace IndiaRose.Services
 {
 	public class TextToSpeechService : ITextToSpeechService
 	{
-		public void ReadText(string text)
+	    private readonly SpeechSynthesizer _ttsSpeechSynthesizer=new SpeechSynthesizer();
+		public async void ReadText(string text)
 		{
+            // The media object for controlling and playing audio.
+            MediaElement mediaElement = new MediaElement();
+
+            // Generate the audio stream from plain text.
+            SpeechSynthesisStream stream = await _ttsSpeechSynthesizer.SynthesizeTextToStreamAsync(text);
+
+            // Send the stream to the media object.
+            mediaElement.SetSource(stream, stream.ContentType);
+            mediaElement.Play();
 		}
 
 		public void Close()
 		{
+            _ttsSpeechSynthesizer.Dispose();
 		}
 	}
 }
