f8c7762cff67743242e02c2146b76942899f1cab
Julien Gauttier <julien.gauttier@gmail.com>
2015-06-01 11:12:54 +0200

    Binding background user

--- Common/IndiaRose.Business/ViewModels/User/UserHomeViewModel.cs
+++ Common/IndiaRose.Business/ViewModels/User/UserHomeViewModel.cs
@@ -3,10 +3,15 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
+using IndiaRose.Interfaces;
+using Storm.Mvvm.Inject;
+using Storm.Mvvm.Services;
 
 namespace IndiaRose.Business.ViewModels.User
 {
     public class UserHomeViewModel : AbstractBrowserViewModel
     {
+
     }
 }
