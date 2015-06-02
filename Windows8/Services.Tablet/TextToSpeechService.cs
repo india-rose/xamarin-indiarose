0ec3a347ee74b12fa2a27a5d3d7988c74f69b512
Valentin <val_jub@live.fr>
2015-06-02 09:57:25 +0200

    sound part

--- Windows8/Services.Tablet/TextToSpeechService.cs
+++ Windows8/Services.Tablet/TextToSpeechService.cs
@@ -5,8 +5,10 @@ using System.Text;
 using System.Threading.Tasks;
 using Windows.Media.PlayTo;
 using Windows.Media.SpeechSynthesis;
+using Windows.Storage;
 using Windows.Storage.Streams;
 using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Media;
 using IndiaRose.Interfaces;
 using IndiaRose.Data.Model;
 using Storm.Mvvm.Inject;
@@ -18,39 +20,45 @@ namespace IndiaRose.Services
 	    private readonly SpeechSynthesizer _ttsSpeechSynthesizer=new SpeechSynthesizer();
         // The media object for controlling and playing audio.
         private MediaElement _sound;
-		public async void ReadText(string text)
-		{
-
-            // Generate the audio stream from plain text.
-            SpeechSynthesisStream stream = await _ttsSpeechSynthesizer.SynthesizeTextToStreamAsync(text);
-
-            // Send the stream to the media object.
-            if (_sound != null)
-            {
-                _sound.Stop();
-            }
-                _sound = new MediaElement();
-                _sound.SetSource(stream, stream.ContentType);            
-                _sound.Play();
-		}
 
 	    public event EventHandler SpeakingCompleted;
 
 	    public void Close()
-		{
+        {
+            _sound.Stop();
             _ttsSpeechSynthesizer.Dispose();
 		}
 
-		public void PlayIndiagram(Indiagram indiagram)
+		public async void PlayIndiagram(Indiagram indiagram)
 		{
             if (indiagram.HasCustomSound)
             {
-                LazyResolver<IMediaService>.Service.PlaySound(indiagram.SoundPath);
+                var audioFile = await StorageFile.GetFileFromPathAsync(indiagram.SoundPath);
+                var stream = await audioFile.OpenAsync(FileAccessMode.Read);
+                PlayStream(stream,"");
             }
             else
             {
-                ReadText(indiagram.Text);
+                SpeechSynthesisStream stream = await _ttsSpeechSynthesizer.SynthesizeTextToStreamAsync(indiagram.Text);
+                PlayStream(stream, stream.ContentType);
             }
 		}
+
+	    private void PlayStream(IRandomAccessStream stream, string mimetype)
+	    {
+            if (_sound==null||_sound.CurrentState != MediaElementState.Playing)
+            {
+                _sound = new MediaElement();
+                _sound.MediaEnded += _sound_MediaEnded;
+                _sound.SetSource(stream,mimetype);
+                _sound.Play();
+            }
+	    }
+
+        private void _sound_MediaEnded(object sender, Windows.UI.Xaml.RoutedEventArgs e)
+        {
+            if (SpeakingCompleted != null)
+                SpeakingCompleted(this, null);
+        }
 	}
 }
