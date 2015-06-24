b684a3fed467a165d0ce5741e6b254f03db7dd31
Valball <val_jub@live.fr>
2015-06-24 11:21:31 +0200

    secondary tile (to test with windows phone)

--- Windows8/Application.Tablet/App.xaml.cs
+++ Windows8/Application.Tablet/App.xaml.cs
@@ -74,7 +74,8 @@ namespace IndiaRose.Application
                 // Quand la pile de navigation n'est pas restaurée, accédez à la première page,
                 // puis configurez la nouvelle page en transmettant les informations requises en tant que
                 // paramètre
-                rootFrame.Navigate(typeof(HomePage), e.Arguments);
+                var target = e.TileId == "AdminPage" ? typeof(MainPage) : typeof(HomePage);
+                rootFrame.Navigate(target, e.Arguments);
             }
             // Vérifiez que la fenêtre actuelle est active
             Window.Current.Activate();
--- Windows8/Application.Tablet/Views/User/HomePage.xaml.cs
+++ Windows8/Application.Tablet/Views/User/HomePage.xaml.cs
@@ -4,6 +4,7 @@ using IndiaRose.Interfaces;
 using Storm.Mvvm;
 using Storm.Mvvm.Inject;
 using Storm.Mvvm.Services;
+using Windows.UI.StartScreen;
 
 // Pour en savoir plus sur le modèle d'élément Page vierge, consultez la page http://go.microsoft.com/fwlink/?LinkId=234238
 
@@ -26,6 +27,33 @@ namespace IndiaRose.Application.Views.User
                 SettingsService.Loaded += SettingsService_Loaded;
 
             DataContextChanged += HomePage_DataContextChanged;
+            Loaded += HomePage_Loaded;
+        }
+
+        async void HomePage_Loaded(object sender, Windows.UI.Xaml.RoutedEventArgs e)
+        {
+            //secondary tile
+            var secondaryTileId = "AdminPage";
+            if (!SecondaryTile.Exists(secondaryTileId))
+            {
+                Uri square150x150Logo = new Uri("ms-appx:///Assets/logoIndiaRose.png");
+                string tileActivationArguments = secondaryTileId + " was pinned at = " + DateTime.Now.ToLocalTime().ToString();
+                string displayName = "IndiaRose.Admin";
+
+                TileSize newTileDesiredSize = TileSize.Square150x150;
+
+                SecondaryTile secondaryTile = new SecondaryTile(secondaryTileId,
+                                                                displayName,
+                                                                tileActivationArguments,
+                                                                square150x150Logo,
+                                                                newTileDesiredSize);
+
+                Uri square30x30Logo = new Uri("ms-appx:///images/logoIndiaRose.png");
+                secondaryTile.VisualElements.Square30x30Logo = new Uri("ms-appx:///images/square30x30Tile-sdk.png");
+                secondaryTile.VisualElements.ForegroundText = ForegroundText.Dark;
+
+                await secondaryTile.RequestCreateAsync();
+            }
         }
 
         void HomePage_DataContextChanged(Windows.UI.Xaml.FrameworkElement sender, Windows.UI.Xaml.DataContextChangedEventArgs args)
