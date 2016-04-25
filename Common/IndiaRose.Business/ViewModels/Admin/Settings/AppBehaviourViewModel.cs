f2074b7a275983c2b73e7bfc5e21e7b74d8403bd
Martin <martinchertier@gmail.com>
2016-04-25 13:46:10 +0200

    Liaison view/viewmodel sur AppBehaviour (moche mais fonctionnelle)

--- Common/IndiaRose.Business/ViewModels/Admin/Settings/AppBehaviourViewModel.cs
+++ Common/IndiaRose.Business/ViewModels/Admin/Settings/AppBehaviourViewModel.cs
@@ -1,8 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
+using Storm.Mvvm.Commands;
 
 namespace IndiaRose.Business.ViewModels.Admin.Settings
 {
@@ -14,6 +17,7 @@ namespace IndiaRose.Business.ViewModels.Admin.Settings
         private bool _categoryReading;
         private bool _backButtonActivated;
         private float _delay;
+        private int _sliderValue;
 
         public bool DragAndDrop
         {
@@ -38,6 +42,38 @@ namespace IndiaRose.Business.ViewModels.Admin.Settings
             get { return _delay; }
             set { SetProperty(ref _delay, value); }
         }
+
+        public int SliderValue
+        {
+            get { return _sliderValue; }
+            set
+            {
+                if (SetProperty(ref _sliderValue, value))
+                {
+                    Delay = value / 10.0f;
+                }
+            }
+        }
         #endregion
+
+        public ICommand OkCommand { get; private set; }
+        public ICommand CancelCommand { get; private set; }
+
+        public AppBehaviourViewModel()
+        {
+            OkCommand = new DelegateCommand(OkClicked);
+            CancelCommand = new DelegateCommand(CancelClicked);
+        }
+
+        private void OkClicked()
+        {
+            Debug.WriteLine("Drag&drop = " + DragAndDrop + " CategoryReading = " + CategoryReading + " BackButtonActivated = " + BackButtonActivated + " Delay/SliderValue = " + Delay + " / " + SliderValue);
+        }
+
+        private void CancelClicked()
+        {
+            
+        }
     }
 }
+
