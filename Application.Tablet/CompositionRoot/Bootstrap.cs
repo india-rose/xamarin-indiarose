433353a1a6a9943cdc9b1154f4dcd8eac20fee77
lindsay grignon <glindsay141095@gmail.com>
2016-04-28 15:10:30 +0200

    Ajout dialog pour add indiagram

--- Application.Tablet/CompositionRoot/Bootstrap.cs
+++ Application.Tablet/CompositionRoot/Bootstrap.cs
@@ -6,6 +6,7 @@ using Application.Tablet.Views.Admin;
 using Application.Tablet.Views.Admin.Collection;
 using Application.Tablet.Views.Admin.Settings;
 using Application.Tablet.Views.Dialogs;
+using IndiaRose.Application.Views.Dialogs;
 using IndiaRose.Business;
 using Storm.Mvvm.Inject;
 using ColorPickerDialog = Application.Tablet.Views.Dialogs.ColorPickerDialog;
@@ -32,19 +33,19 @@ namespace Application.Tablet.CompositionRoot
 
 			var dialogs = new Dictionary<string, Type>
             {
-				/*{Dialogs.ADMIN_COLLECTION_SELECTCATEGORY,typeof(SelectCategoryActionDialog)},
-				{Dialogs.ADMIN_COLLECTION_EXPLORECOLLECTION_CATEGORY,typeof(ExploreCollectionCategoryDialog)},
+				{Dialogs.ADMIN_COLLECTION_SELECTCATEGORY,typeof(SelectCategoryActionDialog)},
+				/*{Dialogs.ADMIN_COLLECTION_EXPLORECOLLECTION_CATEGORY,typeof(ExploreCollectionCategoryDialog)},
 				{Dialogs.ADMIN_COLLECTION_EXPLORECOLLECTION_INDIAGRAM,typeof(ExploreCollectionIndiagramDialog)},*/
                 {Dialogs.ADMIN_SETTINGS_RESETSETTINGS,typeof(ResetSettingsDialog)},
-                {Dialogs.ADMIN_SETTINGS_COLORPICKER, typeof(ColorPickerDialog)}
-                /*{{Dialogs.ADMIN_MAILERROR,typeof(MailErrorDialog)},
+                {Dialogs.ADMIN_SETTINGS_COLORPICKER, typeof(ColorPickerDialog)},
+                //{{Dialogs.ADMIN_MAILERROR,typeof(MailErrorDialog)},
                 {Dialogs.ADMIN_COLLECTION_IMAGECHOICE, typeof(ImageChoiceDialog)},
                 {Dialogs.ADMIN_COLLECTION_SOUNDCHOICE, typeof(SoundChoiceDialog)},
                 {Dialogs.ADMIN_COLLECTION_RECORDSOUND,typeof(RecordSoundDialog)},
-                {Dialogs.ADMIN_COLLECTION_DELETECONFIRMATION_CATEGORY,typeof(DeleteCategoryConfirmation)},
+               /* {Dialogs.ADMIN_COLLECTION_DELETECONFIRMATION_CATEGORY,typeof(DeleteCategoryConfirmation)},
                 {Dialogs.ADMIN_COLLECTION_DELETEWARNING_CATEGORY,typeof(DeleteCategoryWarning)},
-                {Dialogs.ADMIN_COLLECTION_DELETEWARNING_INDIAGRAM, typeof(DeleteIndiagramWarningDialog)},
-                {Dialogs.ADMIN_COLLECTION_CHOOSE,typeof(ChooseCategoryDialog)}*/
+                {Dialogs.ADMIN_COLLECTION_DELETEWARNING_INDIAGRAM, typeof(DeleteIndiagramWarningDialog)},*/
+                {Dialogs.ADMIN_COLLECTION_CHOOSE,typeof(ChooseCategoryDialog)}
             };
             
 
