d9c54543620f0ae0dbf661ab0f1b3f9a115d2378
Martin <martinchertier@gmail.com>
2016-05-13 15:51:50 +0200

    Ajout de deux tests : - AddIndiagramTest - DeleteIndiagramTest

--- Test/UITest.Android/Tests.cs
+++ Test/UITest.Android/Tests.cs
@@ -1,7 +1,11 @@
 ﻿using System;
+using System.Diagnostics;
 using System.IO;
 using System.Linq;
+using IndiaRose.Framework.Views;
+using IndiaRose.Interfaces;
 using NUnit.Framework;
+using Storm.Mvvm.Inject;
 using Xamarin.UITest;
 using Xamarin.UITest.Queries;
 using Xamarin.UITest.Android;
@@ -25,22 +29,48 @@ namespace UITest.Android
                 // code if the app is not included in the solution.
 
                 //.ApkFile ("../../../../Android/Application.Android/bin/Debug/org.indiarose.apk")
-                //.LaunchableActivity("md5e9eb850b4bfdb148cd4a09650ab85c90.AdminSplashscreenActivity")
+                .LaunchableActivity("md5e9eb850b4bfdb148cd4a09650ab85c90.AdminSplashscreenActivity")
 
                 .StartApp();
         }
 
-        /*[Test]
-        public void AppLaunches()
+        [Test]
+        public void AddIndiagramTest()
         {
-            app.Screenshot("First screen.");
-        }*/
+            //app.Repl();
+
+            app.Tap("Collection management");
+            app.WaitForElement(c => c.Class("IndiagramBrowserView"));
+
+            int nbIndiagrams = app.Query(c => c.Class("IndiagramView")).Length;
+            app.Tap("Add");
+            app.EnterText(c => c.Class("EditText"), "TestIndiagram");
+            app.DismissKeyboard();
+            app.Tap("Ok");
+
+            app.WaitForElement(c => c.Class("IndiagramBrowserView"));
+            int nbIndiagramsAfterAdd = app.Query(c => c.Class("IndiagramView")).Length;
+            Debug.WriteLine("Add : " + nbIndiagrams + " -> " + nbIndiagramsAfterAdd);
+            Assert.AreEqual(nbIndiagrams + 1, nbIndiagramsAfterAdd);
+        }
 
         [Test]
-        public void DummyTest()
+        public void DeleteIndiagramTest()
         {
-            //Assert.AreEqual(1, 1);
-            app.Repl();
+            app.Tap("Collection management");
+            app.WaitForElement(c => c.Class("IndiagramBrowserView"));
+
+            int nbIndiagrams = app.Query(c => c.Class("IndiagramView")).Length;
+            var temp = app.Query(c => c.Class("IndiagramView"));
+            var ind = temp[temp.Length - 2]; // Le dernier element est la fleche "next", donc on prend celui d'avant
+            app.Tap(ind.Id);
+            app.Tap("Delete");
+            app.Tap("Delete");
+
+            app.WaitForElement(c => c.Class("IndiagramBrowserView"));
+            int nbIndiagramsAfterDelete = app.Query(c => c.Class("IndiagramView")).Length;
+            Debug.WriteLine("Delete : " + nbIndiagrams + " -> " + nbIndiagramsAfterDelete);
+            Assert.AreEqual(nbIndiagrams - 1, nbIndiagramsAfterDelete);
         }
     }
 }
