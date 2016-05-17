e1ec1825089911b34f2323f2bc535a804a91004a
lindsay grignon <glindsay141095@gmail.com>
2016-05-17 09:13:18 +0200

    test reset ecrit mais non fonctionnel

--- Test/UITest.Android/Tests.cs
+++ Test/UITest.Android/Tests.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.IO;
 using System.Linq;
+using IndiaRose.Interfaces;
 using NUnit.Framework;
+using Storm.Mvvm.Inject;
 using Xamarin.UITest;
 using Xamarin.UITest.Queries;
 using Xamarin.UITest.Android;
@@ -25,23 +27,62 @@ namespace UITest.Android
                 // code if the app is not included in the solution.
 
                 //.ApkFile ("../../../../Android/Application.Android/bin/Debug/org.indiarose.apk")
-                //.LaunchableActivity("md5e9eb850b4bfdb148cd4a09650ab85c90.AdminSplashscreenActivity")
+                .LaunchableActivity("md5e9eb850b4bfdb148cd4a09650ab85c90.AdminSplashscreenActivity")
 
                 .StartApp();
         }
 
-        /*[Test]
-        public void AppLaunches()
-        {
-            app.Screenshot("First screen.");
-        }*/
-
         [Test]
         public void DummyTest()
         {
-            //Assert.AreEqual(1, 1);
             app.Repl();
         }
+
+        //Ne fonctionne pas car impossible de recupperer le service : null exception
+        /*[Test]
+        public void SettingResetTest()
+        {
+            ISettingsService settingsService = LazyResolver<ISettingsService>.Service;
+
+            //Set des valeurs par defaut
+            settingsService.TopBackgroundColor = "FF4042FF";
+            LazyResolver<ISettingsService>.Service.BottomBackgroundColor = "FF5060FF";
+            settingsService.SelectionAreaHeight = 60;
+            settingsService.IndiagramDisplaySize = 300;
+            settingsService.FontName = "DroidSans.ttf";
+            settingsService.FontSize = 36;
+            settingsService.IsReinforcerEnabled = false;
+            settingsService.IsDragAndDropEnabled = true;
+            settingsService.IsCategoryNameReadingEnabled = false;
+            settingsService.IsBackButtonEnabled = false;
+            settingsService.IsMultipleIndiagramSelectionEnabled = true;
+            settingsService.TimeOfSilenceBetweenWords = 2.0f;
+            settingsService.ReinforcerColor = "FF6589FF";
+            settingsService.TextColor = "FF000000";
+            settingsService.IsBackButtonEnabled = false;
+
+            //Comportement de l'application
+            app.Tap(c=>c.Button().Text("Application settings"));
+            app.Tap(c=>c.Button().Text("Reset settings"));
+            app.Tap(c=>c.Button().Text("Ok"));
+
+            //verification du reset 
+            Assert.Equals(settingsService.TopBackgroundColor, "#FF3838FF");
+            Assert.Equals(settingsService.BottomBackgroundColor, "#FF73739E");
+            Assert.Equals(settingsService.SelectionAreaHeight, 50);
+            Assert.Equals(settingsService.IndiagramDisplaySize, 128);
+            Assert.Equals(settingsService.FontName, "Consolas");
+            Assert.Equals(settingsService.FontSize, 20);
+            Assert.Equals(settingsService.IsReinforcerEnabled, true);
+            Assert.Equals(settingsService.IsDragAndDropEnabled, false);
+            Assert.Equals(settingsService.IsCategoryNameReadingEnabled, true);
+            Assert.Equals(settingsService.IsBackHomeAfterSelectionEnabled, true);
+            Assert.Equals(settingsService.IsMultipleIndiagramSelectionEnabled, false);
+            Assert.Equals(settingsService.TimeOfSilenceBetweenWords, 1.0f);
+            Assert.Equals(settingsService.ReinforcerColor, "#FFFF00FF");
+            Assert.Equals(settingsService.TextColor, "#FFFFFFFF");
+            Assert.Equals(settingsService.IsBackButtonEnabled, true);
+        }*/
     }
 }
 
