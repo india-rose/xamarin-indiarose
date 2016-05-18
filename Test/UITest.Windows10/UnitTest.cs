caa8d46f1314f41b012fdff599d88d3d487aa31e
Benjamin <Benjamin@BENJAMIN-PC>
2016-05-18 16:00:35 +0200

    Début UITest Windows 10

---
+++ Test/UITest.Windows10/UnitTest.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Xunit;
+
+namespace UITest.Windows10
+{
+    public class UnitFact
+
+    {
+        [Fact]
+        public void PassingTest()
+        {
+            Assert.Equal(4, Add(2, 2));
+        }
+
+        [Fact]
+        public void FailingTest()
+        {
+            Assert.Equal(5, Add(2, 2));
+        }
+
+        int Add(int x, int y)
+        {
+            return x + y;
+        }
+    }
+}
+
