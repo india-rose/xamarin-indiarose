38bdf809e07f3d5fc69798b8de9a19a4d46f1818
Valball <val_jub@live.fr>
2016-06-30 15:58:30 +0200

    Suppresion dialogues commentés

--- Common/IndiaRose.Business/ViewModels/Admin/Settings/SettingsListViewModel.cs
+++ Common/IndiaRose.Business/ViewModels/Admin/Settings/SettingsListViewModel.cs
@@ -11,75 +11,43 @@ namespace IndiaRose.Business.ViewModels.Admin.Settings
     /// VueModèle de la page de menu des préférences
     /// </summary>
 	public class SettingsListViewModel : AbstractViewModel
-	{
-		#region Commands
+    {
+        #region Commands
 
-		public ICommand ApplicationLookCommand { get; private set; }
-		public ICommand IndiagramPropertyCommand { get; private set; }
-		/*public ICommand DragAndDropCommand { get; private set; }
-
-		public ICommand ReadingDelayCommand { get; private set; }
-		public ICommand CategoryReadingCommand { get; private set; }*/
+        public ICommand ApplicationLookCommand { get; private set; }
+        public ICommand IndiagramPropertyCommand { get; private set; }
         public ICommand AppBehaviourCommand { get; private set; }
 
-		public ICommand ResetSettingsCommand { get; private set; }
+        public ICommand ResetSettingsCommand { get; private set; }
 
-		#endregion
+        #endregion
 
-		public SettingsListViewModel()
-		{
-			ApplicationLookCommand = new DelegateCommand(ApplicationLookAction);
-			/*ReadingDelayCommand = new DelegateCommand(ReadingDelayAction);
-			DragAndDropCommand = new DelegateCommand(DragAndDropAction);
-			CategoryReadingCommand = new DelegateCommand(CategoryReadingAction);*/
+        public SettingsListViewModel()
+        {
+            ApplicationLookCommand = new DelegateCommand(ApplicationLookAction);
             AppBehaviourCommand = new DelegateCommand(AppBehaviourAction);
 
             ResetSettingsCommand = new DelegateCommand(ResetSettingsAction);
-			IndiagramPropertyCommand = new DelegateCommand(IndiagramPropertyAction);
-		}
+            IndiagramPropertyCommand = new DelegateCommand(IndiagramPropertyAction);
+        }
 
-		#region Commands implementation
+        #region Commands implementation
 
         /// <summary>
         /// Ouvre la page de réglages des couleurs de fond de l'application et taille des zones de la partie utilisateur
         /// </summary>
-		private void ApplicationLookAction()
-		{
-			NavigationService.Navigate(Views.ADMIN_SETTINGS_APPLICATIONLOOK);
-		}
+        private void ApplicationLookAction()
+        {
+            NavigationService.Navigate(Views.ADMIN_SETTINGS_APPLICATIONLOOK);
+        }
 
         /// <summary>
         /// Ouvre la page de réglages des propriétés des Indiagrams
         /// </summary>
 		private void IndiagramPropertyAction()
-		{
-			NavigationService.Navigate(Views.ADMIN_SETTINGS_INDIAGRAMPROPERTIES);
-		}
-
-        /// <summary>
-        /// Ouvre le dialogue de réglages du drag and drop
-        /// </summary>
-        /*private void DragAndDropAction()
-		{
-			MessageDialogService.Show(Business.Dialogs.ADMIN_SETTINGS_DRAGANDDROP);
-		}
-
-        /// <summary>
-        /// Ouvre le dialogue de réglages de la lecture des catégories
-        /// </summary>
-		private void CategoryReadingAction()
-		{
-			MessageDialogService.Show(Business.Dialogs.ADMIN_SETTINGS_CATEGORYREADING);
-		}
-
-        /// <summary>
-        /// Ouvre le dialogue de réglages du délai de lecture
-        /// </summary>
-		private void ReadingDelayAction()
-		{
-			MessageDialogService.Show(Business.Dialogs.ADMIN_SETTINGS_READINGDELAY);
-		}
-        */
+        {
+            NavigationService.Navigate(Views.ADMIN_SETTINGS_INDIAGRAMPROPERTIES);
+        }
 
         /// <summary>
         /// Ouvre la page de réglages des features de l'application
@@ -93,10 +61,10 @@ namespace IndiaRose.Business.ViewModels.Admin.Settings
         /// Ouvre le dialogue de réinitilisation des réglages
         /// </summary>
         private void ResetSettingsAction()
-		{
-			MessageDialogService.Show(Business.Dialogs.ADMIN_SETTINGS_RESETSETTINGS);
-		}
+        {
+            MessageDialogService.Show(Business.Dialogs.ADMIN_SETTINGS_RESETSETTINGS);
+        }
 
-		#endregion
-	}
+        #endregion
+    }
 }
